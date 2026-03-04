3d0050cd1984814be4c28911019a81166be04e66
qqaazz800624 <qqaazz800624@gmail.com>
2026-03-04 11:30:50 +0800

    save a test version

--- commands/estimate_factors.sh
+++ commands/estimate_factors.sh
@@ -5,5 +5,5 @@ python scripts/estimate_factors.py \
     --input_filename "truncated_data_with_rtheta_team.parquet" \
     --prob_table "rtheta_prob_tbl.parquet" \
     --output_dir "/neodata/open_dataset/mlb_data/results" \
-    --output_filename "estimated_factors.csv" \
+    --output_filename "test_estimated_factors.csv" \
     --weights 1.0 2.0 3.0 4.0
