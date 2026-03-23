729e51f9bca82a2e352feae303f2a608879b5ff3
qqaazz800624 <qqaazz800624@gmail.com>
2026-03-23 13:39:12 +0800

    rename the script

---
+++ scripts/bin_check.py
@@ -0,0 +1,35 @@
+#%%
+
+import numpy as np
+
+speed_bins = np.arange(0, 121, 3)
+angle_bins = np.arange(-90, 91, 3)
+
+
+#%%
+
+angle_bins.shape
+
+
+#%%
+
+angle_bins
+
+
+
+#%%
+
+
+
+
+#%%
+
+
+
+
+#%%
+
+
+
+
+#%%
