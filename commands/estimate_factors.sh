6bd7f22be874f2baeac9e7be1f20ca19b9e36e6c
D11948002 <d11948002@ntu.edu.tw>
2026-03-28 14:25:57 +0800

    update data path

--- commands/estimate_factors.sh
+++ commands/estimate_factors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python scripts/estimate_factors.py \
-    --data_dir "/neodata/open_dataset/mlb_data/preprocessed" \
+    --data_dir "data/preprocessed" \
     --input_filename "truncated_data_with_rtheta_team.parquet" \
     --prob_table "rtheta_prob_tbl.parquet" \
-    --output_dir "/neodata/open_dataset/mlb_data/results" \
+    --output_dir "data/results" \
     --output_filename "test_estimated_factors.csv" \
     --weights 1.0 2.0 3.0 4.0
